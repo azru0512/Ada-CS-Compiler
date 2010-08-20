3abafe8d711c3461577f6471114b0ed6c82c9621
chenwj <chenwj@cs.nctu.edu.tw>
2010-08-20 16:35:55 +0800

    Move doxygen documents into doc/

--- include/Doxygen/CodeGen.hpp
+++
@@ -1,9 +0,0 @@
-/** \page codegen 產生中介碼
- *
- * 參考文件:
- *
- * <a href="http://gnuu.org/2009/09/18/writing-your-own-toy-compiler/">Writing Your Own Toy Compiler Using Flex, Bison and LLVM</a>
- *
- * <a href="http://llvm.org/docs/tutorial/">LLVM Tutorial</a>
- *
- */ 
