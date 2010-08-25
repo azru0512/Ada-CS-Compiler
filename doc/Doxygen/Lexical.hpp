b4c46bf9e84ce03447c7dc6f6b314dcf21951b13
azru0512 <azru0512@users.sourceforge.net>
2010-08-25 17:42:40 +0800

    修改文件

--- doc/Doxygen/Lexical.hpp
+++ doc/Doxygen/Lexical.hpp
@@ -1,11 +1,11 @@
 /** \page lexical 語彙分析
  *
+ * 語彙分析 (Lexical Analysis) 使用<a href="http://flex.sourceforge.net/">Flex</a>。另外使用<a href="http://www.gnu.org/software/bison/">Bison</a>定義 Flex 傳回記號 (Token) 的值。 
  *
  * 參考文件:
- * 
- * <a href="http://gnuu.org/2009/09/18/writing-your-own-toy-compiler/">Writing Your Own Toy Compiler Using Flex, Bison and LLVM</a>
- *
- * <a href="http://llvm.org/docs/tutorial/">LLVM Tutorial</a>
- *
+ * - <a href="http://www.ibm.com/developerworks/cn/linux/sdk/lex/index.html">Yacc 与 Lex 快速入门</a>
+ * - <a href="http://gnuu.org/2009/09/18/writing-your-own-toy-compiler/">Writing Your Own Toy Compiler Using Flex, Bison and LLVM</a>
+ * - <a href="http://flex.sourceforge.net/manual/">Lexical Analysis With Flex</a>
+ * - <a href="http://www.gnu.org/software/bison/manual/html_node/index.html">Bison 2.4.3</a>
  *
  */ 
--- doc/Doxygen/Preperation.hpp
+++ doc/Doxygen/Preperation.hpp
@@ -3,18 +3,20 @@
  *  <a href="http://git-scm.com/">git</a></br> - 版本管理使用 git。
  *  - \subpage git
  *
- *  <a href="http://www.lrde.epita.fr/~adl/autotools.html">Autotools</a></br> - 我們利用 Autotools 來管理此計劃。
+ *  <a href="http://www.lrde.epita.fr/~adl/autotools.html">Autotools</a></br> - 使用 Autotools 建置此計劃。
  *  - \subpage autotools
  *
- *  Flex/Bison - 使用 Flex 產生 scanner，Bison 產生 parser。
+ *  Flex/Bison - 使用 Flex 產生掃瞄器 (Scanner)，Bison 產生剖析器 (Parser)。
  *  - \subpage flex-bison
  *
  *  C/C++ - Flex/Bison 的部份使用 C。其餘部份使用 C++。
  *  - \subpage cplusplus
  *
  *  <a href="http://www.boost.org/">Boost</a></br> - 儘可能利用 Boost 函式庫。
+ *  - \subpage boost
  *
- *  <a href="http://llvm.org/">LLVM</a></br> - 我們將以 LLVM 作為編譯器後端。
+ *  <a href="http://llvm.org/">LLVM</a></br> - 以 LLVM 作為編譯器後端。
+ *  - \subpage llvm
  *
- *  <a href="http://www.stack.nl/~dimitri/doxygen/">Doxygen</a></br> - 我們使用 Doxygen 對程式碼產生文檔。
+ *  <a href="http://www.stack.nl/~dimitri/doxygen/">Doxygen</a></br> - 使用 Doxygen 產生文檔。
  */
