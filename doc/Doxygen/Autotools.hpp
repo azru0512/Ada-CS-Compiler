573f2551acc4b7367fe0be924a1a5a6d881ab4c9
azru0512 <azru0512@users.sourceforge.net>
2010-08-25 13:54:29 +0800

    修改說明文件

--- doc/Doxygen/Autotools.hpp
+++ doc/Doxygen/Autotools.hpp
@@ -1,15 +1,20 @@
 /** \page autotools Autotools
  *
- * 稍微了解 Makefile 有助於後續工作。請見 <a href="http://tetralet.luna.com.tw/index.php?op=ViewArticle&articleId=185">Makefile 語法簡介</a>。
+ * 稍微了解 Makefile 有助於後續工作。請見<a href="http://tetralet.luna.com.tw/index.php?op=ViewArticle&articleId=185">Makefile 語法簡介</a>。本計劃採用 Autotools 來建置系統。請見<a href="http://tetralet.luna.com.tw/index.php?op=ViewArticle&articleId=200&blogId=1">利用 Autotools 來建立 Makefile 檔案</a>。 執行根目錄底下 autogen.sh 會依序呼叫 libtoolize aclocal autoconf automake 產生 Makefile 和 configure。
  *
- * 本計劃採用 Autotools 來自動產生 Makefile。請見 <a href="http://tetralet.luna.com.tw/index.php?op=ViewArticle&articleId=200&blogId=1">利用 Autotools 來建立 Makefile 檔案</a>。
+ * <a href="http://www.gnu.org/software/hello/manual/autoconf/">Autoconf</a> 裡跟本計劃有關的章節。
+ * - <a href="http://www.gnu.org/software/hello/manual/autoconf/Particular-Programs.html#Particular-Programs">5.2.1 Particular Program Checks</a>
  *
- * 執行根目錄底下 autogen.sh 會依序呼叫 libtoolize aclocal autoconf automake 產生 Makefile 和 configure。
+ * <a href="http://www.gnu.org/software/hello/manual/automake/">Automake</a> 裡跟本計劃有關的章節。
+ * - <a href="http://www.gnu.org/software/hello/manual/automake/Subdirectories.html#Subdirectories">7.1 Recursing subdirectories</a>
+ * - <a href="http://www.gnu.org/software/hello/manual/automake/Program-and-Library-Variables.html#Program-and-Library-Variables">8.4 Program and Library Variables</a>
+ * - <a href="http://www.gnu.org/software/hello/manual/automake/Program-Variables.html#Program-Variables">8.7 Variables used when building a program</a>
+ * - <a href="http://www.gnu.org/software/hello/manual/automake/Yacc-and-Lex.html#Yacc-and-Lex">8.8 Yacc and Lex support</a>
+ * - <a href="http://www.gnu.org/software/hello/manual/automake/C_002b_002b-Support.html#C_002b_002b-Support">8.9 C++ Support</a>
  *
  * 參考文件:
- *
- * <a href="http://www.gnu.org/software/hello/manual/automake/">GNU Automake</a>
- *
- * <a href="http://sourceware.org/autobook/">GNU AUTOCONF AUTOMAKE, AND LIBTOOL</a>
+ * - <a href="http://www.gnu.org/software/hello/manual/autoconf/">Autoconf</a>
+ * - <a href="http://www.gnu.org/software/hello/manual/automake/">Automake</a>
+ * - <a href="http://sourceware.org/autobook/">GNU AUTOCONF AUTOMAKE, AND LIBTOOL</a>
  *
  */
---
+++ doc/Doxygen/Git.hpp
@@ -0,0 +1,14 @@
+/** \page git Git
+ *
+ *  本計劃使用<a href="http://git-scm.com/">git</a>作為版本控制系統，並使用<a href="https://github.com/">GitHub</a>作為遠端倉儲。如何在<a href="https://github.com/">GitHub</a>建立遠端倉儲請見<a href="http://help.github.com/">help.github</a>。
+ *
+ *  免費的線上專案管理系統:
+ *  - <a href="https://github.com/">GitHub</a>
+ *  - <a href="http://code.google.com/intl/en/projecthosting/">Google Code</a>
+ *  - <a href="https://sourceforge.net/">SourceForge</a>
+ *
+ *  參考文件:
+ *  - <a href="http://progit.org/book/zh/">Pro Git 简体中文版</a>
+ *  - <a href="http://www.bitsun.com/documents/gittutorcn.htm">Git 中文教程</a>
+ *  - <a href="http://svnbook.red-bean.com/">Subversion 与版本控制</a> 
+ */ 
