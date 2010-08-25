c5c1d72b56440bfaeed499438fa668b17d4ae06d
chenwj <chenwj@cs.nctu.edu.tw>
2010-08-26 02:31:21 +0800

    建立文檔

--- doc/Doxygen/Boost.hpp
+++ doc/Doxygen/Boost.hpp
@@ -3,8 +3,9 @@
  * 本計劃使用到的 Boost 函式庫:
  * - <a href="http://www.boost.org/doc/libs/release/doc/html/foreach.html">Foreach</a>
  * - <a href="http://www.boost.org/doc/libs/release/libs/conversion/lexical_cast.htm">Lexical Cast</a>
- * - <a href="http://www.boost.org/doc/libs/1_44_0/libs/ptr_container/doc/ptr_container.html">Pointer Container</a>  
+ * - <a href="http://www.boost.org/doc/libs/1_44_0/libs/ptr_container/doc/ptr_container.html">Pointer Container</a>
  * - <a href="http://www.boost.org/doc/libs/release/doc/html/program_options.html">Program Options</a>
+ * - <a href="http://www.boost.org/doc/libs/release/libs/smart_ptr/smart_ptr.htm">Smart Pointers</a>
  * - <a href="http://www.boost.org/doc/libs/release/doc/html/string_algo.html">String Algo</a>  
  *
  */ 
